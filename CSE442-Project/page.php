df5eb7cec4b07a9ea71072d336ed9bb20c50fca9
Hana B Palliyalil <hanabash@timberlake.cse.buffalo.edu>
2019-06-05 17:29:03 -0400

    Generate and send random 6 digit confirmation code

--- CSE442-Project/page.php
+++ CSE442-Project/page.php
@@ -13,13 +13,15 @@ echo "A confirmation has been sent to $email<br>";
 // Remove all illegal characters from email
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
+$random_code = mt_rand(100000, 999999); // Example from https://www.expertsphp.com
+
 // Validate e-mail
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
     echo("$email is a valid email address");
 } else {
     echo("$email is not a valid email address");
 }
-mail($email, "Confirmation", "Your Confirmation code is : X9h7J4");//calls mail() function to send along to the given email --Zack
+mail($email, "Confirmation", "Your Confirmation code is : $random_code");//calls mail() function to send along to the given email --Zack
 	?>
 </body>
 </html>
