ed13ece7aa7e95cdf867b73c09dd108a61544f86
Zachary Perini <ztperini@buffalo.edu>
2019-06-05 14:30:37 -0400

    Email Sanitization and Validation Filtering
    Added the above so that illegal chars stripped out along with whitespace and the correct email format taken. Not sure if this is redundant along with the client-side validation done by HTML.

--- page.php
+++ page.php
@@ -2,25 +2,23 @@
 <html>
 <body>
 <?php
-echo "test email";
-/*
-//form validation attempt-->DID NOT WORK YET Zack
-//Code here mostly taken from https://www.w2schools.com/php/php_form_validation.asp"
-$email = "";
-if ($_SERVER['REQUEST_METHOD']== 'POST'){
-	$email = input_validation($_POST)["address"];
-}
-echo "email";
-function input_validation($data){
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	return $data;
-}
-echo $data;*/
+echo "test email<br>";
 
 $email = $_POST["address"]; //takes the email given to the server via HTTP POST request
 
+/* Below example taken from
+*https://www.w3schools.com/php/filter_validate_email.asp
+*/
+
+// Remove all illegal characters from email
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+// Validate e-mail
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo("$email is a valid email address");
+} else {
+    echo("$email is not a valid email address");
+}
 mail($email, "Confirmation", "Your Confirmation code is : X9h7J4");//calls mail() function to send along to the given email --Zack
 	?>
 </body>
