7ce23e948feba528c344659297a8c69d67debcac
Zachary Perini <ztperini@buffalo.edu>
2019-06-20 03:43:30 -0400

    Added whitespace stripping of confirmation code to actionBasedOnCode.php
    Based on answer found at : https://stackoverflow.com/questions/1279774/how-can-strip-whitespaces-in-phps-variable

--- actionBasedOnCode.php
+++ actionBasedOnCode.php
@@ -9,6 +9,8 @@ if(!empty($_POST["code"])) {
   $code = $_POST["code"];
 }
 
+$code=preg_replace('/\s+/', '', $code);
+
 #$code = $_POST["code"];
 #$return_function = checkCode($code); //print statement for testing --ztperini
 #echo $return_function;
